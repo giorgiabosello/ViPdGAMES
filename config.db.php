517c0dd008c5c090aed8ee069c00f42d9f312958
KingJohn89 <johnmink89@hotmail.it>
2015-09-01 17:47:34 +0200

    Aggiunta password per l'accesso al server
    Non dovrebbe servire, ma per sicurezza la metto

--- config.db.php
+++ config.db.php
@@ -1,6 +1,6 @@
 <?php
 // Connessione
-$dbconn = pg_connect("host=localhost dbname=ViPdGAMES user=postgres password=")
+$dbconn = pg_connect("host=localhost dbname=ViPdGAMES user=postgres password=john")
     or die('Impossibile connettersi: ' . pg_last_error());
 
 
