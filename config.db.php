2e93889c311f9721221a82734fb7c9821dc0125b
Giorgia <gio.bosello@gmail.com>
2015-09-10 19:10:03 +0200

    Eliminati inutili

--- config.db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connessione
-$dbconn = pg_connect("host=localhost dbname=ViPdGAMES user=postgres password=john")
-    or die('Impossibile connettersi: ' . pg_last_error());
-
-
-// Chiusura connessione
-//pg_close($dbconn);
-?>
